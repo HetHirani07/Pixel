05fb33d28c3649702a26b84b9b28e76fe08cd3c0
HetHirani07 <hethirani75@outlook.com>
2025-04-19 15:26:17 +0530

    record

--- src/app/api/pagePixel/[id]/route.ts
+++ src/app/api/pagePixel/[id]/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest } from 'next/server';
 import { PageProps } from '@/app/pageProps';
 
-export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
+export async function GET(req: NextRequest, { params }: { params: Record<string, string> }) {
   const page = PageProps.find((i) => i.id.toString() === params.id);
 
   const forwardedFor = req.headers.get('x-forwarded-for');
